3ae639bfb7bca712cb129e8a3956a448b6ed0f0d
SuBtItRi <dolcedavivolk@gmail.com>
2025-02-14 00:26:18 +0300

    fix

--- src/api.ts
+++ src/api.ts
@@ -17,30 +17,15 @@ export const getRandomText = async () => {
     }
 }
 
-async function fetchAndProcessLargeFile(url) {
-    const response = await fetch(url)
-    const reader = response.body.getReader()
-    const decoder = new TextDecoder('utf-8')
-    let result = ''
-
-    while (true) {
-        const { done, value } = await reader.read()
-        if (done) break
-
-        // Обработка каждой части данных
-        const chunk = decoder.decode(value, { stream: true })
-        result += chunk
-        console.log('Received chunk:', chunk.length)
-    }
-
-    console.log('File fully loaded')
-    return JSON.parse(result) // Преобразуйте результат в JSON
-}
-
 export const getRandomWord = async () => {
-    fetchAndProcessLargeFile(
-        'https://media.githubusercontent.com/media/SuBtItRi/testtyping/main/words.json',
-    )
-        .then((data) => console.log('Parsed data:', data))
-        .catch((error) => console.error('Error:', error))
+    try {
+        const response = await fetch(`http://localhost:3053/words/${getRandomNumber(1, 1532628)}`)
+        if (!response.ok) {
+            throw new Error('Wtf, you stupid man!')
+        }
+        const result = await response.json()
+        return result.word
+    } catch {
+        return 'error'
+    }
 }
