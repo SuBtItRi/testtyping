924785a437f5cbd7a193b4efa290e80d640bc345
SuBtItRi <dolcedavivolk@gmail.com>
2025-02-14 00:33:17 +0300

    Fix Node version and dependencies

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const db = {
   ...require('../db.json'), // Загружаем db.json
-  ...require('../words.json') // Загружаем words.json
+//   ...require('../words.json') // Загружаем words.json
 };
 const router = jsonServer.router(db); // Используем объединенные данные
 const middlewares = jsonServer.defaults();
