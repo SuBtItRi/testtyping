95a4f741f63d41f12288305574aeda4b5f7e32e8
SuBtItRi <dolcedavivolk@gmail.com>
2025-02-14 01:10:56 +0300

    m

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,13 +1,31 @@
-import { create, router as _router, defaults } from 'json-server';
-const server = create();
+const jsonServer = require('json-server');
+const server = jsonServer.create();
 const db = {
-  ...require('./db.json'), // Загружаем db.json
-  ...require('./words.json') // Загружаем words.json
+  ...require('../../db.json'), // Загружаем db.json
+  ...require('../../words.json') // Загружаем words.json
 };
-const router = _router(db); // Используем объединенные данные
-const middlewares = defaults();
+const router = jsonServer.router(db); // Используем объединенные данные
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-export const handler = server;
+exports.handler = async (event, context) => {
+  return new Promise((resolve, reject) => {
+    server.listen(() => {
+      const { url, method, headers, body } = event;
+      const req = { url, method, headers, body };
+      const res = {
+        statusCode: 200,
+        setHeader: (name, value) => headers[name] = value,
+        end: (data) => resolve({
+          statusCode: 200,
+          body: data,
+          headers
+        })
+      };
+
+      server.emit('request', req, res);
+    });
+  });
+};
