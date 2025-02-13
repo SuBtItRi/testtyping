db0bd7d6485776a13e2da09ac28d079a89d43bb4
SuBtItRi <dolcedavivolk@gmail.com>
2025-02-14 00:53:53 +0300

    commit

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,8 +1,8 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const db = {
-  ...require('../db.json'), // Загружаем db.json
-  ...require('../words.json') // Загружаем words.json
+  ...require('./db.json'), // Загружаем db.json
+  ...require('./words.json') // Загружаем words.json
 };
 const router = jsonServer.router(db); // Используем объединенные данные
 const middlewares = jsonServer.defaults();
